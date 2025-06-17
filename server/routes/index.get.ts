e4eb263f86388537d871a26855457177b744f7ef
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-06-17 23:50:53 +0200

    feat: Add order processing handler to fetch and update order statuses

---
+++ server/routes/index.get.ts
@@ -0,0 +1,21 @@
+import { getOdooOrders, updateOrderStatus } from '~/utils/odoo/connect';
+import { getShippingboOrder } from '~/utils/shippingbo/connect';
+
+export default defineEventHandler(async (event) => {
+    const orders = await getOdooOrders();
+
+    for (const order of orders) {
+      try {
+        const shippingbo = await getShippingboOrder(order.shippingbo_id);
+
+        if(shippingbo.state === 'shipped') {
+          await updateOrderStatus(order.id, 'done');
+          break;
+        }
+      } catch (error) {
+        console.error(`Error fetching Shippingbo order for ID ${order.shippingbo_id}:`, error);
+      }
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
+    return orders;
+});
