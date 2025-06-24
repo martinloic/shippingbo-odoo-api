de4886a7a160411115902f4a1b5d7cd4edacc673
Loïc MARTIN <martin.loic.prod@gmail.com>
2025-06-24 10:45:24 +0200

    feat: Implement order status update for 'shipped' state in webhook handler

--- server/routes/webhook/order/state.post.ts
+++ server/routes/webhook/order/state.post.ts
@@ -1,3 +1,5 @@
+import { updateOrderStatus } from '~/utils/odoo';
+
 export default defineEventHandler(async (event) => {
   console.log('Processing new order state from webhook...');
   if (event) {
@@ -5,6 +7,12 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     const newOrderState = body.object;
 
+    if(newOrderState != null) {
+      if(newOrderState.state === 'shipped') {
+        const updatedOrder = await updateOrderStatus(newOrderState.id, newOrderState.tracking_url, 'done');
+        return updatedOrder;
+      }
+    }
     console.log(newOrderState);
   } else {
     console.log('No new order state found in the request body.');
